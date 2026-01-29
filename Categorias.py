2d585da130483fee4f6a05d8e1659c612b6f16d4
Daniela Buendia <danielabuendia@icloud.com>
2026-01-28 18:37:49 -0600

    Mi primer objeto

---
+++ Categorias.py
@@ -0,0 +1,13 @@
+from Categoria import Categoria
+
+Drama = Categoria(1, "Drama", "película", "drama", "Películas dramáticas que exploran emociones profundas.")
+Accion = Categoria(2, "Acción", "película", "acción", "Películas llenas de acción y aventuras emocionantes.")
+Aventura = Categoria(3, "Aventura", "videojuego", "aventura", "Videojuegos que llevan al jugador a explorar mundos nuevos y emocionantes.")
+Comedia = Categoria(4, "Comedia", "película", "comedia", "Películas diseñadas para hacer reír y entretener.")
+RPG = Categoria(5, "RPG", "videojuego", "rol", "Videojuegos de rol donde los jugadores asumen personajes y completan misiones.")
+Terror = Categoria(6, "Terror", "película", "terror", "Películas que buscan asustar y crear suspenso.")
+Deportes = Categoria(7, "Deportes", "videojuego", "deportes", "Videojuegos basados en deportes populares.")
+CienciaFiccion = Categoria(8, "Ciencia Ficción", "película", "ciencia ficción", "Películas que exploran conceptos futuristas y tecnológicos.")
+Estrategia = Categoria(9, "Estrategia", "videojuego", "estrategia", "Videojuegos que requieren planificación y tácticas para superar desafíos.")
+Musical = Categoria(10, "Musical", "película", "musical", "Películas que integran música y canciones en la narrativa.")
+
